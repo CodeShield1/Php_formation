acdb61b3b26d3436bd5f1a3e1e1506f122c611a6
ibrahim_medkour <medkourbrahim08@gmail.com>
2025-02-03 13:51:37 +0100

    exemple date et time

---
+++ Sessions/index.php
@@ -0,0 +1,14 @@
+<?php session_start();?>
+
+
+<html>
+    <body>
+        <form action="profil.php" method='post'  >
+<input type="text" name='name'  placeholder="fname">
+<input type="text" name ='age' placeholder="age"> 
+<input type="submit" name="btn" value='login'>
+
+</form>
+
+</body>
+</html>
